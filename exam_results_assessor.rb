cf82b77eaa5528e8a5ee0a2837c943a8e7e12942
Domita <dominikasiek@gmail.com>
2017-10-22 14:53:10 +0200

    Add exam_results_assessor

---
+++ exam_results_assessor.rb
@@ -0,0 +1,37 @@
+class ExamResultsAssessor
+
+  def initialize(student_results, success_rate_threshold)
+    @student_results        = student_results
+    @success_rate_threshold = success_rate_threshold.to_i
+  end
+
+  def assess
+    passed_students, failed_students = {}, {}
+    
+    @student_results.each do |name, score|
+      passed_students[name] = score if passed_exam?(score)     
+      failed_students[name] = score if failed_exam?(score)    
+    end
+
+    {
+      :passed => passed_students,
+      :failed => failed_students
+    }
+  
+  end
+
+  private
+
+  def passed_exam?(score)
+    score >= @success_rate_threshold
+  end
+
+  def failed_exam?(score)
+    score < @success_rate_threshold
+  end
+
+end
+
+ExamResultsAssessor.new({"Mark" => 10, "Ellen" => 65, "Roger" => 20, "Mike" => 70},"65").assess
+
+
