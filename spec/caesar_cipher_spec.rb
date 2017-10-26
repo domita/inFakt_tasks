48f680c6a52fec4b91d6620932a5da5c080aac60
Domita <dominikasiek@gmail.com>
2017-10-26 12:32:22 +0200

    Unit tests for CaesarCipher

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,73 @@
+require './../code/caesar_cipher'
+
+describe CaesarCipher do
+  describe "#perform" do
+
+    subject do
+      CaesarCipher.new(text_to_encrypt, rot).perform
+    end
+
+    context "when empty string" do
+      let(:text_to_encrypt) {""}
+      let(:rot) {5}
+      it "returns empty string" do
+        encrypted_text = ""
+        expect(subject).to eq(encrypted_text)
+      end
+    end
+
+    context "when string contain mix of special characters and uppercase and lowercase letters" do
+
+      context "and shifted is small" do
+        let(:text_to_encrypt) {"@ #BLTgZ"}
+        let(:rot) {5}
+        it "returns encrypted text" do
+          encrypted_text = "@ #GQYlE"
+          expect(subject).to eq(encrypted_text)
+        end
+      end
+
+      context "and shifted is big" do
+        let(:text_to_encrypt) {"@ #BLTgZ"}
+        let(:rot) {32}
+        it "returns encrypted text" do
+          encrypted_text = "@ #HRZmF"
+          expect(subject).to eq(encrypted_text)
+        end
+      end
+
+    end
+
+    context "when string is long and contain number" do
+
+      context "and shifted is small" do
+        let(:text_to_encrypt) {"Very long string contain 6 words"}
+        let(:rot) {5}
+        it "returns encrypted text" do
+          encrypted_text = "Ajwd qtsl xywnsl htsyfns 6 btwix"
+          expect(subject).to eq(encrypted_text)
+        end
+      end
+
+      context "and shifted is big" do
+        let(:text_to_encrypt) {"Very long string contain 6 words"}
+        let(:rot) {32}        
+        it "returns encrypted text" do
+          encrypted_text = "Bkxe rutm yzxotm iutzgot 6 cuxjy"
+          expect(subject).to eq(encrypted_text)
+        end
+      end  
+
+    end
+
+    context "when string contain one char and shift has default value" do
+      subject do
+          CaesarCipher.new("T").perform
+      end
+      it "returns encrypted text" do
+          encrypted_text = "G"
+          expect(subject).to eq(encrypted_text)
+      end
+    end
+  end
+end
